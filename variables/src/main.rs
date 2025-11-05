f55a1b1f0d9fe142f7d6c6c7932a700f4ca3cb46
Sunny ☀️ <sanay.nair@gmail.com>
2025-11-05 14:15:30 +0000

    chapter 3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let sum = my_function(32, -32);
+    println!("The sum is: {}", sum);
+}
+
+fn my_function(x: i32, y: i32) -> i32 {
+    println!("x = {}\ny = {}", x, y);
+    x+y
 }
