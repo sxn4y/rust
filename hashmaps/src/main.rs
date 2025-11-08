c2cf2dcdc5fa535a1a7ba5d3775bc3eeef497afd
Sunny ☀️ <sanay.nair@gmail.com>
2025-11-08 10:15:16 +0000

    collections

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,19 +1,47 @@
 use std::collections::HashMap;
 
+// fn main() {
+//     let blue = String::from("Blue");
+//     let yellow = String::from("Yellow");
+
+//     let mut scores = HashMap::new();
+
+//     scores.insert(blue, 10);
+//     scores.insert(yellow, 50);
+
+//     // let team_name = String::from("Blue");
+//     // let score = match scores.get(&team_name) {
+//     //     Some(&score) => score,
+//     //     None => 0,
+//     // };
+
+//     for (key, value) in &scores {
+//         println!("{}: {}", key, value);
+//     }
+
+//     println!("{:#?}", scores);
+// }
+
+// fn main() {
+//     let mut scores = HashMap::new();
+
+//     scores.insert("Blue", 20);
+
+//     // scores.insert("Blue", 10); // for forced mutation
+//     scores.entry("Blue").or_insert(10); // inserts if key "Blue" does not have a value
+
+//     println!("{:#?}", scores);
+// }
+
 fn main() {
-    let blue = String::from("Blue");
-    let yellow = String::from("Yellow");
+    let text = "hello world wonderful world";
 
-    let mut scores = HashMap::new();
+    let mut map = HashMap::new();
 
-    scores.insert(blue, 10);
-    scores.insert(yellow, 50);
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
 
-    let team_name = String::from("Blue");
-    let score = match scores.get(&team_name) {
-        Some(&score) => score,
-        None => 0,
-    };
-
-    println!("{:#?}", scores);
+    println!("{:#?}", map);
 }
